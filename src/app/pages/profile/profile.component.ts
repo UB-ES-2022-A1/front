8cfcd35a6a7a379f3cf3734b659fd4c2436b1d83
Sergi Fernández <sergixghost@gmail.com>
2022-11-24 13:43:35 +0100

    styles setup 1

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -11,80 +11,88 @@ import { ContractedServicesService } from 'src/app/services/contracted-services/
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.sass']
 })
 export class ProfileComponent implements OnInit {
   isEditable: boolean = false;
   user: any = {
-    username: '', 
+    username: '',
     email: '',
-    phone: null
-  }
+    phone: null,
+  };
   newName: string = '';
   newPhone: any = null;
   offers: ServiceTO[] = [];
   contracts: ServiceTO[] = [];
 
-
-  constructor(private modalService: NgbModal, protected sessionService: SessionService, private userService: UserService,private serviceService: ServiceService, private contractedService: ContractedServicesService) { }
+  constructor(
+    private modalService: NgbModal,
+    protected sessionService: SessionService,
+    private userService: UserService,
+    private serviceService: ServiceService,
+    private contractedService: ContractedServicesService
+  ) {}
   ngOnInit(): void {
-    this.userService.getUser(this.sessionService.get('email')).subscribe((data: any) =>{
-      this.user.username = data.name
-      this.user.email = data.email
-      this.user.phone = data.phone
-    })
+    this.userService
+      .getUser(this.sessionService.get('email'))
+      .subscribe((data: any) => {
+        this.user.username = data.name;
+        this.user.email = data.email;
+        this.user.phone = data.phone;
+      });
   }
-  
+
   enableEdit() {
     this.isEditable = !this.isEditable;
   }
 
-  clearEdit(){
+  clearEdit() {
     this.isEditable = !this.isEditable;
     this.newName = '';
     this.newPhone = null;
   }
 
   openCreateService() {
-    const modalRef = this.modalService.open(FormServiceComponent,  { centered: true });
+    const modalRef = this.modalService.open(FormServiceComponent, {
+      centered: true,
+    });
   }
-  
+
   saveProfile() {
-    this.userService.putUser(this.newName,this.user.email,this.newPhone).subscribe(res =>{
-      console.log(res); 
-      console.log('saveprofile'); 
-      //this.sessionService.set('username', this.user.username);
-    })
+    this.userService
+      .putUser(this.newName, this.user.email, this.newPhone)
+      .subscribe((res) => {
+        console.log(res);
+        console.log('saveprofile');
+        //this.sessionService.set('username', this.user.username);
+      });
   }
 
   loadOffers(): void {
-    this.serviceService.getUserServices(this.user.email).subscribe(res => {
-      console.log('loading'); 
-      res.forEach((service: any) =>{
-        let auxService: ServiceTO = 
-        {
+    this.serviceService.getUserServices(this.user.email).subscribe((res) => {
+      console.log('loading');
+      res.forEach((service: any) => {
+        let auxService: ServiceTO = {
           id: service.id,
-          title: service.title, 
+          title: service.title,
           description: service.description,
-          price: service.price 
-        }
-        this.offers.push(auxService); 
-      })
-    })
+          price: service.price,
+        };
+        this.offers.push(auxService);
+      });
+    });
   }
 
   loadContracts(): void {
-    this.contractedService.getUserContract(this.user.email).subscribe(res => {
-      res.forEach((service: any) =>{
-        let auxService: ServiceTO = 
-        {
+    this.contractedService.getUserContract(this.user.email).subscribe((res) => {
+      res.forEach((service: any) => {
+        let auxService: ServiceTO = {
           id: service.id,
-          title: service.title, 
+          title: service.title,
           description: service.description,
-          price: service.price 
-        }
-        this.contracts.push(auxService); 
-      })
-    })
+          price: service.price,
+        };
+        this.contracts.push(auxService);
+      });
+    });
   }
 }
