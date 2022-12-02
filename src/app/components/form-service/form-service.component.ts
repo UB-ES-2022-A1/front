c5433555ef480e232ed5b87738d099ed1749478c
Sergi Fernández <sergi.fernandez@basetis.com>
2022-12-02 09:41:25 +0100

    fixed naviagation after saving service

--- src/app/components/form-service/form-service.component.ts
+++ src/app/components/form-service/form-service.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ServiceService } from 'src/app/services/service/service.service';
 import { SessionService } from 'src/app/services/session/session.service';
+import { UtilsService } from 'src/app/services/utils/utils.service';
 
 @Component({
   selector: 'app-form-service',
@@ -15,7 +17,9 @@ export class FormServiceComponent implements OnInit {
   constructor(
     private serviceService: ServiceService,
     private modalService: NgbModal,
-    private sessionService: SessionService
+    private sessionService: SessionService,
+    private router: Router,
+    private utils: UtilsService
   ) {}
 
   ngOnInit(): void {
@@ -55,12 +59,19 @@ export class FormServiceComponent implements OnInit {
           this.sessionService.get('email')
         )
         .subscribe(
-          (res: any) => {
+          (res) => {
             console.log(res);
+            this.router
+              .navigate([`/service/${res.modified_service_id}`])
+              .then(() => {
+                window.location.reload();
+              });
             this.modalService.dismissAll();
+            this.utils.openSnackBar('Service modified correctly', 'Ok', 0);
           },
           (error) => {
             console.log(error);
+            this.utils.openSnackBar('Error after modifying service', 'Ok', 1);
           }
         );
     }
